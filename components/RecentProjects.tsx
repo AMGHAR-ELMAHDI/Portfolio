38abd92b65e9db6c29fe31e14f6f20da1f2526ad
AMGHAR ELMAHDI <amgharmahdi1@gmail.com>
2024-08-06 14:22:26 +0100

    almost donme with projects section need to fix small screen

--- components/RecentProjects.tsx
+++ components/RecentProjects.tsx
@@ -1,8 +1,22 @@
+"use client";
 import { projects } from "@/data";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { PinContainer } from "./ui/3d-pin";
+import { FaLocationArrow } from "react-icons/fa";
 
 function RecentProjects() {
+  const [width, setWidth] = useState<number>(window.innerWidth);
+
+  function handleWindowSizeChange() {
+    setWidth(window.innerWidth);
+  }
+  useEffect(() => {
+    window.addEventListener("resize", handleWindowSizeChange);
+    return () => {
+      window.removeEventListener("resize", handleWindowSizeChange);
+    };
+  }, []);
+
   return (
     <section id="projects">
       <div className="py-20">
@@ -10,14 +24,14 @@ function RecentProjects() {
           A small selection of{" "}
           <span className="text-purple"> Recent projects </span>
         </h1>
-        <div className=" flex flex-wrap items-center justify-center p-4 gap-16 mt-10">
+        <div className=" flex flex-wrap items-center justify-center p-4 gap-x-24 gap-y-8 mt-10">
           {projects.map((project, i) => (
             <div
               key={i}
-              className=" lg:min-h-[32.5rem] border border-red-200 h-[25rem] flex items-center justify-center sm:w-96 w-[80vw]"
+              className="sm:h-[41rem] h-[32rem] lg:min-h-[32.5rem] flex items-center justify-center sm:w-[570px] w-[80vw]"
             >
-              <PinContainer title={project.title} href={project.link} className="space-y-3">
-                <div className="relative flex items-center justify-center sm:w-96 w-[80vw] overflow-hidden h-[20vh] lg:h-[30vh]">
+              <PinContainer title={project.link} href={project.link}>
+                <div className="relative flex items-center justify-center sm:w-[570px] w-[80vw] overflow-hidden sm:h-[40vh] h-[30vh]  mb-10">
                   <div className="relative w-full h-full overflow-hidden lg:rounded-3xl bg-[#13162d]">
                     <img src="/bg.png" alt="bg" />
                   </div>
@@ -33,6 +47,27 @@ function RecentProjects() {
                 <p className="lg:text-xl lg:font-normal font-light text-sm line-clamp-2">
                   {project.des}
                 </p>
+                <div className="flex items-center justify-between mt-7 mb-3">
+                  <div className="flex items-center">
+                    {project.iconLists.map((icon, i) => (
+                      <div
+                        key={i}
+                        className="border border-white/[0.2] rounded-full bg-black lg:w-10 lg:h-10 w-8 h-8 flex items-center justify-center"
+                        style={{ transform: `translateX(-${5 * i * 2}px)` }}
+                      >
+                        <img src={icon} alt={icon} className="p-2" />
+                      </div>
+                    ))}
+                  </div>
+                  <div className="flex justify-center items-center">
+                    {width > 768 && (
+                      <p className="lg:text-xl md:text-xs text-xs text-purple">
+                        View the project
+                      </p>
+                    )}
+                    <FaLocationArrow className="text-purple ms-3" />
+                  </div>
+                </div>
               </PinContainer>
             </div>
           ))}
